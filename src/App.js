ea060290d63c3350b41e03ab37398b51f10412a9
joshex150 <joshex150@gmail.com>
2023-08-25 10:50:13 +0100

    light-dark mode

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./layouts/Main"; // fallback for lazy pages
 import "./static/css/main.scss"; // All of our styles
+import "./app.css"; // All of our styles
 
 const { PUBLIC_URL } = process.env;
 
