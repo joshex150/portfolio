2dc9e779b5109bf43ec260fbd70b596f3a442c75
joshex150 <joshex150@gmail.com>
2023-09-10 19:56:11 +0100

    disabling certain keys

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from "react";
+import React, { Suspense, lazy, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./layouts/Main"; // fallback for lazy pages
 import "./static/css/main.scss"; // All of our styles
@@ -7,7 +7,7 @@ import "./app.css"; // All of our styles
 const { PUBLIC_URL } = process.env;
 
 // Every route - we lazy load so that each page can be chunked
-// NOTE that some of these chunks are very small. We should optimize
+// NOTE that some of these chunks are very small. should be optimized
 // which pages are lazy loaded in the future.
 const About = lazy(() => import("./pages/About"));
 const Contact = lazy(() => import("./pages/Contact"));
@@ -16,19 +16,54 @@ const NotFound = lazy(() => import("./pages/NotFound"));
 const Projects = lazy(() => import("./pages/Projects"));
 const Stats = lazy(() => import("./pages/Stats"));
 
-const App = () => (
-  <BrowserRouter basename={PUBLIC_URL}>
-    <Suspense fallback={<Main />}>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/stats" element={<Stats />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Suspense>
-  </BrowserRouter>
-);
+const App = () => {
+  useEffect(() => {
+    // Disable right-click
+    const handleContextMenu = (e) => {
+      e.preventDefault();
+    };
+
+    // Function to check Ctrl, Option (Alt), and Command (Windows key)
+    const handleKeyDown = (e) => {
+      if (e.ctrlKey || e.altKey || e.metaKey) {
+        e.preventDefault();
+      }
+
+      // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+      if (
+        e.keyCode === 123 ||
+        (e.ctrlKey && e.shiftKey && e.keyCode === "I".charCodeAt(0)) ||
+        (e.ctrlKey && e.shiftKey && e.keyCode === "J".charCodeAt(0)) ||
+        (e.ctrlKey && e.keyCode === "C".charCodeAt(0)) ||
+        (e.ctrlKey && e.keyCode === "U".charCodeAt(0))
+      ) {
+        e.preventDefault();
+      }
+    };
+
+    document.addEventListener("contextmenu", handleContextMenu);
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      // Remove event listeners when the component unmounts
+      document.removeEventListener("contextmenu", handleContextMenu);
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  });
+  return (
+    <BrowserRouter basename={PUBLIC_URL}>
+      <Suspense fallback={<Main />}>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/stats" element={<Stats />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,42 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import Main from "../layouts/Main";
 
-const Index = () => (
-  <Main
-    description={
-      "JOSHUA CHRISTIAN's personal website. New York based Stanford ICME graduate" +
-      ", VP of Engineering at Smile Identity, co-founder of Arthena and Matroid, and YC Alumni."
-    }
-  >
-    <article className="post" id="index">
-      <header>
-        <div className="title">
-          <h2>Welcome 🧍‍♂️</h2>
-        </div>
-      </header>
-      <header>
-        <div className="title">
-          <h2>
-            <Link to="/">About this site</Link>
-          </h2>
-          <p>
-            A Stunning, Responsive, Statically-Generated React App Crafted with
-            Modern JavaScript.
-          </p>
-        </div>
-      </header>
-      <p>
-        {" "}
-        Welcome to my website. Please feel free to read more{" "}
-        <Link to="/about">about me</Link>, checkout my{" "}
-        <Link to="/projects">projects</Link>, view{" "}
-        <Link to="/stats">site statistics</Link>, or{" "}
-        <Link to="/contact">contact</Link> me.
-      </p>
-    </article>
-  </Main>
-);
+const Index = () => {
+  return (
+    <Main
+      description={
+        "JOSHUA CHRISTIAN's personal website. New York based Stanford ICME graduate" +
+        ", VP of Engineering at Smile Identity, co-founder of Arthena and Matroid, and YC Alumni."
+      }
+    >
+      <article className="post" id="index">
+        <header>
+          <div className="title">
+            <h2>Welcome 🧍‍♂️</h2>
+          </div>
+        </header>
+        <header>
+          <div className="title">
+            <h2>
+              <Link to="/">About this site</Link>
+            </h2>
+            <p>
+              A Stunning, Responsive, Statically-Generated React App Crafted
+              with Modern JavaScript.
+            </p>
+          </div>
+        </header>
+        <p>
+          {" "}
+          Welcome to my website. Please feel free to read more{" "}
+          <Link to="/about">about me</Link>, checkout my{" "}
+          <Link to="/projects">projects</Link>, view{" "}
+          <Link to="/stats">site statistics</Link>, or{" "}
+          <Link to="/contact">contact</Link> me.
+        </p>
+      </article>
+    </Main>
+  );
+};
 
 export default Index;
