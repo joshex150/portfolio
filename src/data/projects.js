7caeebeee9189a55fc3fa562e38bebe48aa866b8
joshex150 <joshex150@gmail.com>
2024-09-26 17:13:38 +0100

    changes

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,10 +1,16 @@
 // TODO Add a couple lines about each project
+import fox from "../assets/projects/foxcom.png";
+import dashboard from "../assets/projects/primar.webp";
+import huddle from "../assets/projects/huddle.webp";
+import zibara from "../assets/projects/zibara.webp";
+import pWeb from "../assets/projects/Primar-web.webp";
+import pMark from "../assets/projects/Primar-mark.webp";
 const data = [
   {
     title: "Primar Market (Launched January 2024)",
     subtitle: "A delivery store.",
     link: "https://primar-market.vercel.app",
-    image: "/images/projects/Primar-mark.webp",
+    image: pMark,
     date: "2023-12-11",
     desc: "Primar is a delivery technology company based in Nigeria, dedicated to bridging the gap between farmers, markets, and customers. We specialize in delivering farm-fresh products from local farmers and markets directly to customers in close proximity, ensuring that you have access to the freshest, healthiest, and most sustainably sourced produce right at your doorstep, all with unprecedented convenience.",
   },
@@ -12,7 +18,7 @@ const data = [
     title: "Primar Meals (in progress)",
     subtitle: "An online meal plan store.",
     link: "https://prima-web.vercel.app",
-    image: "/images/projects/Primar-web.webp",
+    image: pWeb,
     date: "2023-08-28",
     desc: "Immerse yourself in the world of Primar, a cutting-edge delivery store, meticulously crafted using React-JS and CSS. With a tech stack that includes a variety of essential tools, this project showcases my prowess in frontend development. From its sleek user interface to its intuitive user experience, Primar sets a new standard for online delivery platforms.",
   },
@@ -20,7 +26,7 @@ const data = [
     title: "Zibara",
     subtitle: "Shopping site",
     link: "https://zibara.web.app/",
-    image: "/images/projects/zibara.webp",
+    image: zibara,
     date: "2020-04-20",
     desc: "Explore a captivating shopping experience through my  first commisioned work, where Firebase and React converge to create an innovative online store. Witness the fusion of Firebase's dynamic backend and React's interactive frontend. From seamless user authentication to real-time cart updates, this project showcases the synergy of these technologies. Discover a responsive design, smooth checkout process, and an intuitive admin dashboard. With secure transactions and a user-centric interface, this project epitomizes the modern shopping journey.",
   },
@@ -28,14 +34,14 @@ const data = [
     title: "Huddle",
     subtitle: "Fintech",
     link: "https://huddle-chris.vercel.app",
-    image: "/images/projects/huddle.webp",
+    image: huddle,
     date: "2022-05-20",
     desc: "showcasing the groundbreaking fintech startup 'Huddle.' Crafted using React-TS for a robust frontend and ExpressJS and NodeJS for a dynamic backend, this project redefines financial interactions. Harnessing the power of MongoDB for data management, Vite for lightning-fast performance, and SCSS and CSS for stunning visuals, Huddle exemplifies innovation. Explore secure user authentication, real-time transaction tracking, and responsive design. With Huddle, I've woven together technology and finance for a revolutionary user experience.",
   },
   {
     title: "Primar Dashboard",
     subtitle: "A store management dashboard.",
-    image: "/images/projects/primar.webp",
+    image: dashboard,
     date: "2023-06-28",
     desc: "Dive into the dynamic world of store management with 'Primar,' a cutting-edge dashboard designed to streamline operations. The backend is powered by an array of technologies including ExpressJS, MongoDB for data storage, and Socket.IO for real-time communication. Utilizing tools like JSON Web Tokens, Nodemailer, and Crypto-JS, the backend is fortified with security and seamless functionality. The frontend, built with React, showcases a responsive and intuitive user interface. With Framer Motion for engaging animations, React Router for smooth navigation, and Vite for optimized performance, Primar offers an unparalleled user experience. Explore 'Primar' and witness the fusion of backend prowess and frontend finesse.",
   },
@@ -43,7 +49,7 @@ const data = [
     title: "Foxcom Synergy LTD",
     subtitle: "An oil and Gas industry servicing company",
     link: "https://foxcomsynergy.com",
-    image: "/images/projects/foxcom.png",
+    image: fox,
     date: "2023-06-28",
     desc: "The best delivering services to the oil and gas industry EPCI, Oil Spill Recovery and Environmental Control, as well as leasing of heavy Equipment, Manpower consultancy, Subsea Engineering not just for today but for Generations to come.",
   },
